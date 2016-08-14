6214c2b06dc241f9757002968a80059946b720af
nuanda31 <nuandafr@hotmail.com>
2016-08-14 03:07:50 +0200

    Ajout du test sur une led

--- rpi-test1/src/main/java/fr/nsoft/application/controller/BaseController.java
+++ rpi-test1/src/main/java/fr/nsoft/application/controller/BaseController.java
@@ -3,6 +3,12 @@ package fr.nsoft.application.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
 @RestController
 public class BaseController {
 
@@ -11,4 +17,24 @@ public class BaseController {
 		return "Rapsberry Pi 3 services";
 	}
 	
+	@RequestMapping("/led")
+	public String lightLed() {
+		GpioController controller = null;
+		try {
+			controller = GpioFactory.getInstance();
+			GpioPinDigitalOutput digitalOutput = controller.provisionDigitalOutputPin(RaspiPin.GPIO_01, "PIN_LED", PinState.HIGH);
+			digitalOutput.setShutdownOptions(true, PinState.LOW);
+			Thread.sleep(2000);
+			digitalOutput.toggle();
+			Thread.sleep(2000);
+			digitalOutput.toggle();
+		} catch (InterruptedException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if (controller != null) {
+				controller.shutdown();
+			}
+		}
+		return "";
+	}
 }
